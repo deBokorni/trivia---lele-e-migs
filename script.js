6863b2d141bd49467607e2de25e99d5850483f6e
deBokorni <afonssoleandro@gmail.com>
2025-11-14 21:31:42 -0300

    variaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+//variaveis
+let currentQuestionIndex = 0;
+let score = 0;
+let triviaData = [];
+const NUMBER_OF_QUESTIONS = 5; // Mínimo de 5 questões
+
+const gameArea = document.getElementById('game-area');
+const difficultySelection = document.getElementById('difficulty-selection');
+const triviaContainer = document.getElementById('trivia-container');
+const scoreContainer = document.getElementById('score-container');
+
